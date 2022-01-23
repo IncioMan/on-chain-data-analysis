476cd504ce64a763fd63df5d44ade7ae9390fb87
Alex Incerti <alex.incerti@outlook.com>
2022-01-23 17:08:55 +0100

    ...

--- prism_launch/charts.py
+++ prism_launch/charts.py
@@ -9,8 +9,8 @@ from data import user_stats_df,hourly_stats_df, wallet_age_df, hourly_new_users_
 txs_over_time_chart = alt.Chart(hourly_stats_df).mark_bar().encode(
     x=alt.X(cols_dict['HR']+':T', axis=alt.Axis(
                         tickCount=10, labelAngle=0, tickBand = 'center')),
-    y=cols_dict['TOT_TXS'],
-    tooltip=[cols_dict['HR']+':T',cols_dict['TOT_TXS']]
+    y=cols_dict['TOT_TXS']+":Q",
+    tooltip=[cols_dict['HR']+':T',cols_dict['TOT_TXS']+":Q"]
 ).configure_mark(
     color='#ffde85'
 ).configure_view(strokeOpacity=0)
@@ -18,16 +18,16 @@ txs_over_time_chart = alt.Chart(hourly_stats_df).mark_bar().encode(
 users_over_time_chart = alt.Chart(hourly_new_users_df).mark_bar().encode(
     x=alt.X(cols_dict['TIME']+':T', axis=alt.Axis(tickCount=10, 
                                                 labelAngle=0, tickBand = 'center')),
-    y=cols_dict['cumsum_new_users'],
-    tooltip=[cols_dict['TIME']+':T', cols_dict['cumsum_new_users']]
+    y=cols_dict['cumsum_new_users']+":Q",
+    tooltip=[cols_dict['TIME']+':T', cols_dict['cumsum_new_users']+":Q"]
 ).configure_mark(
     color='#fab0ba'
 ).configure_view(strokeOpacity=0)
 ####
 users_dep_distr_chart = alt.Chart(deposits_bucket_df.rename(columns=cols_dict).sort_values(by='BUCKET')).mark_bar().encode(
-    y=alt.X(cols_dict['bucket_name'], sort=alt.EncodingSortField(order='ascending')),
-    x=cols_dict['N_USERS'],
-    tooltip=[cols_dict['bucket_name'],cols_dict['N_USERS']]
+    y=alt.X(cols_dict['bucket_name']+":N", sort=alt.EncodingSortField(order='ascending')),
+    x=cols_dict['N_USERS']+":Q",
+    tooltip=[cols_dict['bucket_name']+":N",cols_dict['N_USERS']+":Q"]
 ).configure_mark(
     color='#fab0ba'
 ).properties(height=300).configure_axisX(
@@ -36,8 +36,8 @@ users_dep_distr_chart = alt.Chart(deposits_bucket_df.rename(columns=cols_dict).s
 ####
 cum_ust_chart = alt.Chart(hourly_stats_df.rename(columns=cols_dict)).mark_line(point=True).encode(
     x=alt.X(cols_dict['HR']+':T', sort=alt.EncodingSortField(order='ascending')),
-    y=cols_dict['cumsum_ust'],
-    tooltip=[cols_dict['HR']+':T',cols_dict['cumsum_ust']]
+    y=cols_dict['cumsum_ust']+":Q",
+    tooltip=[cols_dict['HR']+':T',cols_dict['cumsum_ust']+":Q"]
 ).configure_mark(
     color='#fab0ba'
 ).properties(width=700).configure_axisX(
@@ -47,33 +47,37 @@ cum_ust_chart = alt.Chart(hourly_stats_df.rename(columns=cols_dict)).mark_line(p
 df = user_stats_df.rename(columns=cols_dict)[cols_dict['DEPOSIT_TXS']]\
     .value_counts().sort_index().reset_index().rename(columns={'index':cols_dict['DEPOSIT_TXS'],cols_dict['DEPOSIT_TXS']:'N° of users'})
 n_tx_wallet_chart = alt.Chart(df).mark_line(point = True).encode(
-    y='N° of users',
-    x=cols_dict['DEPOSIT_TXS'],
-    tooltip=['N° of users',cols_dict['DEPOSIT_TXS']]
-)
+    y=alt.Y('N° of users:O', sort="descending"),
+    x=cols_dict['DEPOSIT_TXS']+":O",
+    tooltip=['N° of users:O',cols_dict['DEPOSIT_TXS']+":Q"]
+).properties(height=300).configure_view(strokeOpacity=0)
 ####
 user_part_prev_launches_chart = alt.Chart(prev_launches_df).mark_bar().encode(
-x=alt.X(cols_dict['TYPE'], axis=alt.Axis(labelAngle=0, tickBand = 'center')),
-y=cols_dict['PARTICIPANTS'],
+x=alt.X(cols_dict['TYPE']+":N", axis=alt.Axis(labelAngle=0, tickBand = 'center')),
+y=cols_dict['PARTICIPANTS']+":Q",
 color=alt.Color(cols_dict['PARTICIPATE_TYPE'],legend=alt.Legend(
     orient='none',
     padding=10,
     legendY=-10,
     direction='horizontal')),
-tooltip=[cols_dict['TYPE'],cols_dict['PARTICIPANTS'],cols_dict['PARTICIPATE_TYPE']]
+tooltip=[cols_dict['TYPE']+":N",cols_dict['PARTICIPANTS']+":Q",cols_dict['PARTICIPATE_TYPE']]
 ).properties(height=400).configure_axisX(
 labelAngle=-10
 ).configure_view(strokeOpacity=0)
 ####
-dep_dist_balance_chart =alt.Chart(deposit_balance_df.sample(n=5000, random_state=1)).mark_point().encode(
-y=cols_dict['AMOUNT'],
-x=cols_dict['AVG_BALANCE_USD'],
+if(len(deposit_balance_df)>5000):
+    df = deposit_balance_df.sample(n=5000, random_state=1)
+else:
+    df = deposit_balance_df
+dep_dist_balance_chart =alt.Chart(df).mark_point().encode(
+y=cols_dict['AMOUNT']+":Q",
+x=cols_dict['AVG_BALANCE_USD']+":Q",
 color=alt.Color(cols_dict['N_TXS'],
     scale=alt.Scale(scheme='yelloworangered')),
 tooltip=[cols_dict['SENDER'], cols_dict['AMOUNT'],
     cols_dict['AVG_BALANCE_USD'],
     cols_dict['N_TXS']]
-).interactive()
+).configure_view(strokeOpacity=0).interactive()
 ###
 wallet_age_df = wallet_age_df.rename(columns=cols_dict)
 df2 = wallet_age_df.head()
@@ -81,7 +85,7 @@ df2['fake'] = 100
 df2
 c = alt.Chart(wallet_age_df).mark_bar(color='#ffde85').encode(
     x=alt.X(cols_dict['MIN_DATE']+":T", axis=alt.Axis(tickCount=10, labelAngle=0)),
-    y=cols_dict['ADDRESS_COUNT'],
+    y=cols_dict['ADDRESS_COUNT']+":Q",
     tooltip=[cols_dict['MIN_DATE'],cols_dict['ADDRESS_COUNT']]
 )
 
--- prism_launch/data.py
+++ prism_launch/data.py
@@ -1,46 +1,74 @@
 import pandas as pd
 from constants import cols_dict
-def claim(claim_hash):
-    # load claims into df
-    df_claim = pd.read_json(
-        f"https://api.flipsidecrypto.com/api/v2/queries/{claim_hash}/data/latest",
-        convert_dates=["BLOCK_TIMESTAMP"],
-    )
 
-    return df_claim
 
 user_stats = '499224b4-30a6-43d7-80b9-3a019cbb1d3d'
 deposits_bucket = 'b4953cda-a874-43fa-b78d-ceb0c1bfc3cf'
 deposit_balance = '9e2e9587-0850-466a-8a59-4dda2e8337f3'
 hourly_new_users = '65179a1e-fd70-43eb-a9e4-ce14b716c928'
+wallet_age = 'fb964d4e-3e67-4662-b9b1-413262737bcb'
+hourly_stats = '520fb3b6-a968-4742-bf0a-31cbb67b6b05'
+prev_launches = '22bf0295-9733-46d6-ab98-1cb753552c6b'
+
+cols_claim = {
+    user_stats : ['DEPOSIT_AMOUNT', 'DEPOSIT_TXS', 'SENDER', 'WITHDRAWN_AMOUNT',
+       'WITHDRAW_TXS'],
+    deposits_bucket : ['BUCKET', 'N_USERS'],
+    prev_launches : ['PARTICIPANTS', 'PARTICIPATE_TYPE', 'TYPE'],
+    hourly_stats : ['DEPOSIT_AMOUNT', 'DEPOSIT_TX', 'DEP_USERS', 'HR', 'NET_AMOUNT',
+       'TOT_TXS', 'TOT_USERS', 'WITH_AMOUNT', 'WITH_TX', 'WITH_USERS'],
+    wallet_age : ['ADDRESS_COUNT', 'MIN_DATE'],
+    deposit_balance: ['AMOUNT', 'AVG_BALANCE_USD', 'MAX_BALANCE_USD', 'N_TXS', 'SENDER'],
+    hourly_new_users: ['NEW_USERS', 'TIME']
+}
+
+def claim(claim_hash):
+    df_claim = pd.read_json(
+        f"https://api.flipsidecrypto.com/api/v2/queries/{claim_hash}/data/latest",
+        convert_dates=["BLOCK_TIMESTAMP"],
+    )
+    
+    if(len(df_claim.columns)==0):
+        return pd.DataFrame(columns = cols_claim[claim_hash])
+
+    return df_claim
+
 
 user_stats_df = claim(user_stats)
 deposit_balance_df = claim(deposit_balance)
 deposits_bucket_df = claim(deposits_bucket)
+wallet_age_df = claim(wallet_age)
+hourly_stats_df = claim(hourly_stats)
+prev_launches_df = claim(prev_launches)
 
 
 hourly_new_users_df = claim(hourly_new_users)
 hourly_new_users_df['cumsum_new_users'] = hourly_new_users_df.sort_values(by='TIME').NEW_USERS.cumsum()
 df = hourly_new_users_df.sort_values(by='TIME')
 index = df.index
-index[-2]
-next_last_users = df.loc[index[-2]].cumsum_new_users
+if(len(index)>1):
+    i = -2
+    next_last_users = df.loc[index[i]].cumsum_new_users
+else:
+    next_last_users = 0
 hourly_new_users_df = hourly_new_users_df.rename(columns=cols_dict)
 
-wallet_age = 'fb964d4e-3e67-4662-b9b1-413262737bcb'
-wallet_age_df = claim(wallet_age)
+
 wallet_age_df = wallet_age_df.rename(columns=cols_dict)
 
 
-hourly_stats = '520fb3b6-a968-4742-bf0a-31cbb67b6b05'
-hourly_stats_df = claim(hourly_stats)
 hourly_stats_df['cumsum_ust'] = hourly_stats_df.sort_values(by='HR').NET_AMOUNT.cumsum()
 hourly_stats_df['cumsum_txs'] = hourly_stats_df.sort_values(by='HR').TOT_TXS.cumsum()
 df = hourly_stats_df.sort_values(by='HR')
 index = df.index
-index[-2]
-next_last_ust = df.loc[index[-2]].cumsum_ust
-next_last_txs = df.loc[index[-2]].cumsum_txs
+if(len(index)>1):
+    i = -2
+    next_last_ust = df.loc[index[i]].cumsum_ust
+    next_last_txs = df.loc[index[i]].cumsum_txs
+else:
+    i = 0
+    next_last_ust = 0
+    next_last_txs = 0
 hourly_stats_df = hourly_stats_df.rename(columns=cols_dict)
 
 
@@ -48,8 +76,7 @@ n_txs = user_stats_df.DEPOSIT_TXS.sum() + user_stats_df.WITHDRAW_TXS.sum()
 n_users = user_stats_df.SENDER.nunique()
 tot_deposits = int(user_stats_df.DEPOSIT_AMOUNT.sum() - user_stats_df.WITHDRAWN_AMOUNT.sum())
 
-prev_launches = '22bf0295-9733-46d6-ab98-1cb753552c6b'
-prev_launches_df = claim(prev_launches)
+
 prev_launches_df = prev_launches_df.rename(columns=cols_dict)
 
 deposit_balance_df = deposit_balance_df.rename(columns=cols_dict)
